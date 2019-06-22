3983ac8adf081a3d82958f4757440326bfc2d931
harringa0466 <52090501+harringa0466@users.noreply.github.com>
2019-06-21 21:22:50 -0400

    Add files via upload
    first submission

---
+++ P2T1_SalesPredictiction_aaronHarrington.py
@@ -0,0 +1,22 @@
+# In this program it will caculate the profit made from sales
+#6-12-2019
+#CTI-110 P2T1
+#Aaron Harrington
+
+# Pseudcode
+# declare variables
+
+# total_sales = 0.0 float
+# profit = 0.0  float
+
+# promt user to enter projected sales
+
+total_sales= float(input("Enter the projected sales:"))
+
+#calculate the profit as 23 percent of total sales
+
+profit = total_sales * .023
+
+# Dispaly the profit
+
+print("the profit is $", profit)
